a0fd486bab13c5c8ed86a1d1209b0c13136dff00
Blessing <nejo.blessing@yahoo.com>
2024-01-05 13:25:03 -0800

    binary tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,17 +8,18 @@
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+		const binary_tree_t *second)
 {
 	binary_tree_t *mom, *pop;
-	
+
 	if (!first || !second)
 		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
-	
+
 	mom = first->parent, pop = second->parent;
-	return (binary_trees_ancestor(first, pop));
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
 	else if (mom == second || !pop || (!pop->parent && mom))
 		return (binary_trees_ancestor(mom, second));
 	return (binary_trees_ancestor(mom, pop));
